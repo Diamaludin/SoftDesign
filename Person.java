98de8ebd8cde9d30d7651120b3b8fe9bc711eef0
Diamaludin <79895005+Diamaludin@users.noreply.github.com>
2021-03-07 13:39:15 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,9 @@
+1st name: Diamaludin
+Last name: Mariga
+echo "# SoftDesign" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Diamaludin/SoftDesign.git
+git push -u origin main
