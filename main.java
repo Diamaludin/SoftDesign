93f3c9bf0bdf9f316db002a09d34ccb467568a84
Diamaludin <79895005+Diamaludin@users.noreply.github.com>
2021-03-08 17:32:09 +0800

    Update main.java
    5 Persons

--- main.java
+++ main.java
@@ -1,1 +1,26 @@
 
+public class Main {
+  private String fname1 = "John";
+  private String lname1 = "Loyd";
+  private String fname2 = "Neil";
+  private String lname2 = "Buison";
+  private String fname3 = "Diamaludin";
+  private String lname3 = "Mariga";
+  private String fname4 = "reyna";
+  private String lname4 = "Ryu";
+  private String fname5 = "Tris";
+  private String lname5 = "Belle";
+  
+  
+  
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname1 + " " + myObj.lname1);
+      System.out.println("Name: " + myObj.fname2 + " " + myObj.lname2);
+      System.out.println("Name: " + myObj.fname3 + " " + myObj.lname3);
+      System.out.println("Name: " + myObj.fname4 + " " + myObj.lname4);
+      System.out.println("Name: " + myObj.fname5 + " " + myObj.lname5);
+    
+  }
+}
